56dce7b9c6c6d55e2522e5b4b1d6d5568ab7786a
divijkhaitan <divijkhaitan@gmail.com>
2022-06-09 01:22:21 +0530

    QuerySelector will not work, buttons will be added

--- rps.js
+++ rps.js
@@ -1,3 +1,47 @@
+
+const buttons=document.querySelector('div')
+console.log(buttons)
+buttons.forEach(button=>button.addEventListener('click',clicked))
+
+
+function game()
+{
+    let playerscore=0
+    let computerscore=0
+    let computerchoice=""
+    let playerchoice=""
+    while((computerscore<5)&&(playerscore<5))
+    {
+        playerchoice=prompt("Choose your object").toLowerCase()
+        computerchoice=computerplay()
+        
+        let result=round(playerchoice,computerchoice);
+        if(result=="draw")
+        {
+            console.log( "It's a tie! You and your opponent made the same choice")
+        }
+        else if (result=="win")
+        {
+            playerscore++;
+            console.log( `You win! ${playerchoice} beats ${computerchoice}`)
+        }
+        else if (result=="lose")
+        {
+            computerscore++;
+            console.log(`You lose! ${computerchoice} beats ${playerchoice}`)
+        }
+        console.log(result);
+    }
+    if(playerscore>computerscore)
+    {
+        console.log(`You win ${playerscore} - ${computerscore}`)
+    }
+    else
+    {
+        console.log(`You lose ${playerscore} - ${computerscore}`)
+    }
+}
+
 function computerplay()
 {
     let a= Math.ceil(Math.random()*3);
@@ -66,41 +110,7 @@ function round(playerchoice, computerchoice)
     }
     return result;
 }
-
-function game()
+function clicked(event)
 {
-    let playerscore=0
-    let computerscore=0
-    let computerchoice=""
-    let playerchoice=""
-    while((computerscore+playerscore)<5)
-    {
-        playerchoice=prompt("Choose your object").toLowerCase()
-        computerchoice=computerplay()
-    
-        let result=round(playerchoice,computerchoice);
-        if(result=="draw")
-        {
-            console.log( "It's a tie! You and your opponent made the same choice")
-        }
-        else if (result=="win")
-        {
-            playerscore++;
-            console.log( `You win! ${playerchoice} beats ${computerchoice}`)
-        }
-        else if (result=="lose")
-        {
-            computerscore++;
-            console.log(`You lose! ${computerchoice} beats ${playerchoice}`)
-        }
-        console.log(result);
-    }
-    if(playerscore>computerscore)
-    {
-        console.log(`You win ${playerscore} - ${computerscore}`)
-    }
-    else
-    {
-        console.log(`You lose ${playerscore} - ${computerscore}`)
-    }
+    console.log(event);
 }
